aee2f06f57eff6e47e75b955e16cddd235ef9d9c
Yifan Shen <yifan.shen@aberdeen-asset.com>
2019-11-04 19:06:23 +0000

    updating threeSum_closest

--- ThreeSum_Closet/solution_1.py
+++ ThreeSum_Closet/solution_1.py
@@ -1,5 +1,6 @@
 # Given an array nums of n integers and an integer target, 
-# find three integers in nums such that the sum is closest to target. 
+# find three integers in nums 
+# such that the sum is closest to target. 
 # Return the sum of the three integers. 
 # You may assume that each input would have exactly one solution.
 # 
@@ -10,6 +11,7 @@
 # The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
 
 class Solution(object):
+    ## Two pointer solution, O(N^2)
 
     def threeSumClosest(nums, target):
         """
@@ -34,14 +36,39 @@ class Solution(object):
         return closest_sum
 
 ## EXP:
-# Brute-force: O(N^3)
-# 
-# Brute force solution will be O(N^3). We end up testing every subset and update the closest sum in every iteration.
-# Two Pointer Solution: O(N^2)
-# 
-# We can use the two pointer method to reduce complexity to O(N^2). We begin by sorting the array.
-# Now we use three indices i,j and k. We iterate i from 0 to N (actually till N-2 is fine). We initialize j to i+1 and k to N-1.
-# Now we compute curr_sum = nums[i]+nums[j]+nums[k]. If this equals target, we have the closest sum.
-# Otherwise update closest_sum using the rule abs(curr_sum-target) < abs(closest_sum-target).
-# Now what if curr_sum is less than target. Should we test (nums[i]+nums[j]+nums[k-1]), (nums[i]+nums[j]+nums[k-2]), (nums[i]+nums[j]+nums[k-3]) ? The answer is NO. All of these triplets will be less than curr_sum. And curr_sum is less than target - so there is no point testing these triplets. We must move forward by advancing j to j + 1 in the hope to get a larger triplet. This is the main intuition in this problem.
-# You can visualize (6) by thinking all possible triplet sums sorted and arranged on a number line. When you find a curr_sum less than target, you increase curr_sum by increasing j. When you find a curr_sum less more than target, you reduce curr_sum by reducing k
+# Brute-force: O(N^3) 
+# Brute force solution will be O(N^3). 
+# We end up testing every subset 
+# and update the closest sum in every iteration.
+
+# Two Pointer Solution: O(N^2) 
+# We can use the two pointer method to
+# reduce complexity to O(N^2).
+# We begin by sorting the array.
+# Now we use three indices i,j and k. 
+# We iterate i from 0 to N 
+# (actually till N-2 is fine). 
+# We initialize j to i+1 and k to N-1.
+# Now we compute 
+# curr_sum = nums[i]+nums[j]+nums[k]. 
+# If this equals target, 
+# we have the closest sum.
+# Otherwise update closest_sum using the rule 
+# abs(curr_sum-target) < abs(closest_sum-target).
+# Now what if curr_sum is less than target. 
+# Should we test (nums[i]+nums[j]+nums[k-1]), 
+# (nums[i]+nums[j]+nums[k-2]), 
+# (nums[i]+nums[j]+nums[k-3]) ? 
+# The answer is NO. 
+# All of these triplets will be less than curr_sum. 
+# And curr_sum is less than target - 
+# so there is no point testing these triplets. 
+# We must move forward by advancing j to j + 1 
+# in the hope to get a larger triplet. 
+# This is the main intuition in this problem.
+# You can visualize (6) by 
+# thinking all possible triplet sums sorted and arranged on a number line. 
+# When you find a curr_sum less than target, 
+# you increase curr_sum by increasing j. 
+# When you find a curr_sum less more than target, 
+# you reduce curr_sum by reducing k
