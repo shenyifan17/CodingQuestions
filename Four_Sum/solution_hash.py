b28eaf4f6fe70af7d9f9dd7d49151d75c6f78fdf
Yifan Shen <yifan.shen@aberdeen-asset.com>
2019-11-06 19:17:40 +0000

    add a hash solution

---
+++ Four_Sum/solution_hash.py
@@ -0,0 +1,24 @@
+class Solution(object):
+
+    def fourSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[List[int]]
+        """
+        nums.sort()
+        dic = collections.defaultdict(set)
+        res = set()
+        n = len(nums)
+        
+        for i in range(n):
+            for j in range(i + 1, n):
+                sum = nums[i] + nums[j]
+                for half in dic[target - sum]:
+                    res.add(tuple(list(half) + [nums[i], nums[j]]))
+                    
+            for k in range(i):
+                dic[nums[i] + nums[k]].add((nums[k], nums[i]))
+                
+        return list(res)
+
