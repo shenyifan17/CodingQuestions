ec6ebd8347f8dca78e7a8b0367bef70152278a45
Yifan Shen <yifan.shen@aberdeen-asset.com>
2019-11-19 20:17:11 +0000

    adding no_repeat_substring

--- Grokking/Sliding_Window/no_repeat_substring.py
+++ Grokking/Sliding_Window/no_repeat_substring.py
@@ -39,11 +39,36 @@ def solution(str):
     """
     window_start = 0 
     max_length = 0
-    char_index_map = collections.Counter()
+    char_index_map = collections.Counter() # record position 
+    print(f'======================================= {str} ============================= ')
 
     ## try to extend the range [windowStart, windowEnd]
     for window_end in range(len(str)):
         right_char = str[window_end]
+        print(f'window_end = {window_end}, right_char = {right_char}')
+        # if the hash map alread contains right_char,
+        # then shrink the window so that we only have 
+        # one occurance of right_char
+        if right_char in char_index_map:
+            print(f'window_start = {window_start}, char_index_map = {char_index_map}, '\
+                  f'char_index_map[right_char] + 1 = {char_index_map[right_char] + 1}')
+            # in the current window, 
+            # we will not have any right_char after its previous index
+            # and if window_start is alread ahead of the last index of right_char
+            # we will keep window_start 
+            window_start = max(window_start, char_index_map[right_char]+1)
+        # insert/update 'right_char' into the map
+        char_index_map[str[window_end]] = window_end
+        # remember the maximum length so far
+        max_length = max(max_length, window_end - window_start + 1)
+    return max_length
 
 
+def main():
+  print("Length of the longest substring: " + str(solution("aabccbb")))
+  print("Length of the longest substring: " + str(solution("abbbb")))
+  print("Length of the longest substring: " + str(solution("aaabcdefghiigjlqpweoc")))
+  print("Length of the longest substring: " + str(solution("asadqwezxxcvnkjsdffsdfg")))
+  print("Length of the longest substring: " + str(solution("ae1234567asdasd33")))
 
+main()
