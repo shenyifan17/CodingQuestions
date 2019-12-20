5e14aa87504863eb5c917425fed0ae95fda83d59
Yifan Shen <yifan.shen@aberdeen-asset.com>
2019-12-20 16:21:14 +0000

    adding new q

--- Grokking/Binary_Search/minimum_difference_element.py
+++ Grokking/Binary_Search/minimum_difference_element.py
@@ -22,3 +22,55 @@ Example 4:
 Input: [4, 6, 10], key = 17
 Output: 10
 """
+
+
+""""
+Solution:
+We can use a similar approach as discussed in 
+Order-agnostic Binary Search. 
+We will try to search for the ‘key’ in the given array. 
+If we find the ‘key’ we will return it as the 
+minimum difference number. 
+If we can’t find the ‘key’, (at the end of the loop) 
+we can find the differences between the ‘key’ and 
+the numbers pointed out by indices start and end, 
+as these two numbers will be closest to the ‘key’. 
+The number that gives minimum difference will be our required number.
+"""
+
+def search_min_diff(arr, key):
+
+    # boundary condition
+    if key < arr[0]:
+        return arr[0]
+    n = len(arr)
+
+    if key > arr[n-1]:
+        return arr[n-1]
+
+    start, end = 0, n-1
+    while start <= end:
+        mid = start + (end - start) // 2 
+        if key < arr[mid]:
+            end = mid - 1 
+        elif key > arr[mid]:
+            start = mid + 1 
+        else:
+            return arr[mid]
+
+    ## at the end of the while loop:
+    # if we are not able to find the element in the 
+    # given array, we then return the element closet to 'key'
+    if (arr[start] - key) < (key - arr[end]):
+        return arr[start]
+    return arr[end]
+
+
+
+# Time complexity
+# Since, we are reducing the search range by half at every step, 
+# this means the time complexity of our algorithm will be O(logN) 
+# where ‘N’ is the total elements in the given array.
+# 
+# Space complexity
+# The algorithm runs in constant space O(1)
