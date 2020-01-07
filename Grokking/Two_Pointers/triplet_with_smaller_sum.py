251ed30edfe30b8ea4b3a248c8a53387a9f62555
Yifan Shen <yifan.shen@aberdeen-asset.com>
2020-01-07 18:40:06 +0000

    adding q

--- Grokking/Two_Pointers/triplet_with_smaller_sum.py
+++ Grokking/Two_Pointers/triplet_with_smaller_sum.py
@@ -57,7 +57,7 @@ def main():
     print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
     print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))    
 
-main() 
+# main() 
 
 """"
 Time complexity 
@@ -69,4 +69,47 @@ Space complexity
 Ignoring the space required for the output array, 
 the space complexity of the above algorithm will be O(N) 
 which is required for sorting if we are not using an in-place sorting algorithm.
+"""
+
+## Similar Question: 
+# instead of returning count, returna all triplets 
+def triplet_with_smaller_sum_(arr, target):
+    arr.sort() 
+    triplets = [] 
+    for i in range(len(arr) - 2):
+        search_pair_(arr, target - arr[i], i, triplets)
+    return triplets
+
+def search_pair_(arr, target_sum, first, triplets):
+    left = first + 1 
+    right = len(arr) - 1
+    while (left < right):
+        if arr[left] + arr[right] < target_sum: # found the triplet 
+            # since arr[right] >= arr[left], we can replace arr[right] by 
+            # any number between left and right to get a sum less than the target sum 
+            for i in range(right, left, -1):
+                triplets.append([arr[first], arr[left], arr[i]])
+            left += 1 
+        else:
+            right -= 1 # we need a pair with a smaller sum 
+
+def main_():
+    print(triplet_with_smaller_sum([-1, 0, 2, 3], 3))
+    print(triplet_with_smaller_sum([-1, 4, 2, 1, 3], 5))
+
+main_()
+
+
+""""
+Time complexity #
+Sorting the array will take O(N * logN). The searchPair(), in this case, will take O(N^2)
+​the main while loop will run in O(N) but the nested for loop can also take O(N)
+- this will happen when the target sum is bigger than every triplet in the array.
+
+So, overall searchTriplets() will take O(N * logN + N^3)
+​which is asymptotically equivalent to O(N^3)j
+
+Space complexity #
+Ignoring the space required for the output array, 
+the space complexity of the above algorithm will be O(N) which is required for sorting.
 """
